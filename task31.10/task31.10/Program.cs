932694fac696dc88b7fafb62b090af58a0ac8a1b
NigarAdigozelova <72kwbg1@code.edu.az>
2022-10-31 22:59:39 +0400

    created new hometask

---
+++ task31.10/task31.10/Program.cs
@@ -0,0 +1,31 @@
+﻿
+#region 1-10 aralığında olan cüt ədədlərin ədədi ortasını tapan alqoritm yazın.
+//int n = 10;
+//int count = 0;
+//float sum = 0;
+//for (int i = 1; i <= n; i++)
+//{
+//    if (i % 2 == 0)
+//    {
+//        sum = sum + i;
+//        count++;
+//    }
+//}
+//float avarage = sum / count;
+//Console.WriteLine(avarage);
+#endregion
+#region Ədədin mərtəbələrinin cəmini tapan alqoritm (məsələn: 123 - 1+2+3).
+int num = 123;
+int rem = 0;
+int sum = 0;
+
+while (num > 0)
+{
+    rem = num % 10;
+    sum = sum + rem;
+    num = num / 10;   
+}
+Console.WriteLine(sum);
+
+
+#endregion
